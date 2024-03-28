6f7dbb836bcd94f31fea9f8f11e4122964b906ee
emrahkiziltan94 <138603299+emrahkiziltan94@users.noreply.github.com>
2024-03-28 17:54:00 +0300

    solution.

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -3,10 +3,19 @@ package org.example.model;
 public class Store {
     public static void main(String[] args) {
 
-        //listProducts();
+        ProductForSale[] products = new ProductForSale[5];
+        products[0] = new Chocolate("sweet", 13.4, "ch desc", "white", true);
+        products[1] = new Bread("Bakery", 5, "br desc", "einkorn", "brown");
+        products[2] = new Coke("Drinks", 15, "soğuk içiniz", true, 2.5);
+
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            if (product != null) {
+                product.showDetails();
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/rpg/utils/Constant.java
@@ -0,0 +1,13 @@
+package org.example.rpg.utils;
+
+import org.junit.jupiter.engine.Constants;
+
+public class Constant {
+    private Constant() {
+    }
+
+    public static final double POISON_PERCENTAGE = 0.3;
+    public static final double BLEED_PERCENTAGE = 0.25;
+
+
+}
